87652439cdcf6d5652a2fce8a66831d534e21922
Daniel García <dngarcia9319@gmail.com>
2022-02-02 18:43:21 -0600

    Creates boilerplate

---
+++ README.md
@@ -0,0 +1,5 @@
+# Svelte Boilerplate
+
+A boilerplate to create Svelte SPAs with TS, SCSS and linting.
+
+Enable pre-commit hooks by running the `prepare` script.
---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  const title = 'Svelte boilerplate';
+</script>
+
+<main>
+  <h1>{title}</h1>
+</main>
+
+<style lang="scss" global>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  $heading: #2249b4;
+
+  h1 {
+    color: $heading;
+  }
+</style>
